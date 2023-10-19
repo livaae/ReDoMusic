5ed82737b4820d7e44e03bd1fbe8bdccad9c24ce
liva <141638184+livaae@users.noreply.github.com>
2023-10-19 18:35:17 +0300

    ReDoMusic

---
+++ ReDoMusic/Core/ReDoMusic.Domain/Entities/Instrument.cs
@@ -0,0 +1,22 @@
+﻿using ReDoMusic.Domain.Common;
+using ReDoMusic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Entities
+{
+    public class Instrument : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+        public Brand Brand { get; set; } //Enum x,Value Obj x, Entities
+        public string Model { get; set; }
+        public ColorType Color { get; set; }
+        public DateTime? ProductionYear { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
